0f1a541b0c65e3684704bfd8410b9e0506b8bb1e
kaioruan <kaio.ruan@souunit.com.br>
2022-12-27 14:40:07 -0300

    feat: create model Provider

---
+++ backend/src/database/models/Providers.ts
@@ -0,0 +1,155 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Cnpj from './Cnpjs';
+
+class Provider extends Model {
+  id!: number;
+  name!: string;
+  tradingName!: string;
+  cashforceTax!: string;
+  responsibleName!: string;
+  responsibleEmail!: string;
+  responsiblePosition!: string;
+  responsiblePhone!: string;
+  responsibleMobile!: string;
+  website!: string;
+  postalCode!: string;
+  address!: string;
+  number!: string;
+  complement!: string;
+  neighborhood!: string;
+  city!: string;
+  state!: string;
+  bank!: string;
+  bankAgency!: string;
+  account!: string;
+  documents!: string;
+  phoneNumber!: string;
+  situation!: string;
+  situationDate!: string;
+  cnpjId!: number;
+  email!: string;
+}
+
+Provider.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tradingName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cashforceTax: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  responsibleName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  responsibleEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  responsiblePosition: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  responsiblePhone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  responsibleMobile: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  website: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  postalCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bank: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bankAgency: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  account: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  documents: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  complement: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  neighborhood: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  situation: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  situationDate: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cnpjId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'cnpjs',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize: db,
+  modelName: 'providers',
+  underscored: true,
+  timestamps: false,
+  createdAt: true,
+  updatedAt: true,
+});
+Provider.belongsTo(Cnpj, { foreignKey: 'cnpjId', as: 'providers' });
+Cnpj.hasMany(Provider, { foreignKey: 'id', as: 'users' });
+
+export default Provider;
