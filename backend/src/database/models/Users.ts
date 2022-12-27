066734fdfe9ed752d55f1229ec12ab7219524c64
kaioruan <kaio.ruan@souunit.com.br>
2022-12-27 14:03:17 -0300

    feat: create model Users

---
+++ backend/src/database/models/Users.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  name!: string;
+  email!: string;
+  phoneNumber!: string;
+  mobile!: string;
+  departament!: string;
+  verificationCode!: string;
+  emailChecked!: boolean;
+  cashforceAdm!: boolean;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  mobile: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  departament: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  verificationCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  emailChecked: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  cashforceAdm: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  underscored: true,
+  timestamps: false,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export default User;
