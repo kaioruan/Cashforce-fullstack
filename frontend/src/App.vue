87464df3f25b371f45f5d2c77bf59b66447b1bd0
kaioruan <kaio.ruan@souunit.com.br>
2022-12-31 11:47:09 -0300

    feat: start header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,21 @@
-<script setup></script>
+<script setup>
+import Header from "./components/HeaderComponent.vue";
+</script>
 
 <template>
-  <header>
-    <div class="flex-container">
-      <img src="./images/menu-hero.png" />
-    </div>
-  </header>
+  <header></header>
+
+  <main>
+    <Header />
+  </main>
 </template>
 
-<style scoped>
-header {
-  position: absolute;
-  width: 288px;
-  height: 900px;
-  left: 0px;
-  top: 0px;
-}
-
-.flex-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-
-  position: absolute;
-  width: 288px;
-  height: 144px;
-  left: 0px;
-  top: 0px;
-}
-.flex-container img {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-
-  width: 288px;
-  height: 64px;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 1;
-  flex-grow: 0;
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  border: 0;
+  padding: 0;
+  font-family: sans-serif;
 }
 </style>
