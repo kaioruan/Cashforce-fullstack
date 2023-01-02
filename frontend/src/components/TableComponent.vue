2a216c5d31e4600056dd83611e0b37f269b39ca2
kaioruan <kaio.ruan@souunit.com.br>
2023-01-02 15:59:30 -0300

    feat: css table list

--- frontend/src/components/TableComponent.vue
+++ frontend/src/components/TableComponent.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="title">
-    <h3 class="page-header">
-      <img src="../images/handsTable.png" alt="hands icon" /> Notas Fiscais
-    </h3>
-    <p class="title-text">Visualize as notas fiscais que você tem.</p>
-  </div>
-  <div>
-    <table v-if="!isLoading">
+  <div class="container">
+    <div class="title">
+      <h3 class="page-header">
+        <img src="../images/handsTable.png" alt="hands icon" /> Notas Fiscais
+      </h3>
+      <p class="title-text">Visualize as notas fiscais que você tem.</p>
+    </div>
+
+    <table>
       <thead>
-        <tr class="table-headers">
+        <tr class="header">
           <th>NOTA FISCAL</th>
           <th>SACADO</th>
           <th>CEDENTE</th>
           <th>EMISSÃO</th>
           <th>VALOR</th>
           <th>STATUS</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="table in tableData" :key="table.id" class="table-data">
+        <tr v-for="table in tableData" :key="table.id" class="table-container">
           <td>
             {{ table.nNf }}
           </td>
@@ -33,9 +35,7 @@
               table.emissionDate.split("T")[0].split("-").reverse().join("-")
             }}
           </td>
-          <td>
-            {{ table.value }}
-          </td>
+          <td>{{ table.value }}</td>
           <td>
             {{ table.status }}
           </td>
@@ -73,6 +73,14 @@ export default {
     response.data.forEach((element) => {
       element.status = STATUS_NF[Number(element.orderStatusBuyer)];
     });
+    const formatter = new Intl.NumberFormat("pt-br", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    });
+    response.data.forEach((element) => {
+      element.value = formatter.format(element.value);
+    });
     console.log(response.data);
     this.tableData = response.data;
   },
@@ -82,6 +90,7 @@ export default {
 <style scoped>
 .title {
   color: rgb(2, 27, 81);
+  margin-top: 40px;
 }
 .page-header {
   font-weight: bold;
@@ -89,4 +98,56 @@ export default {
 .title-text {
   font-size: 14px;
 }
+table {
+  height: 1vh;
+  width: 80%;
+  border-collapse: separate;
+  border-spacing: 0 1rem;
+}
+/* .container {
+  float: left;
+} */
+
+tr,
+td {
+  font-size: 14px;
+}
+
+.table-container {
+  border-radius: 6px;
+  box-shadow: 0px 0px 1px 1px red;
+}
+
+td,
+th {
+  /* margin-left: 100px; */
+  text-align: start;
+  padding: 10px 10px;
+}
+
+.header th {
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.3);
+}
+
+tr button {
+  padding: 7px 15px;
+  margin-right: -70px;
+  border-radius: 20px;
+  color: rgba(0, 0, 0, 0.8);
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+}
+
+tr td:nth-child(5),
+tr td:nth-child(6) {
+  color: rgb(0, 173, 140);
+}
+
+/* .container {
+  background: red;
+  background-color: red !important;
+  box-shadow: 0px 10px 30px red;
+  border-radius: 15px 0px 0px;
+} */
 </style>
