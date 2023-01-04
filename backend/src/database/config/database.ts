0e1f4c1557c15b2070cccb1a48aa4600dc131227
kaioruan <kaio.ruan@souunit.com.br>
2023-01-04 15:51:40 -0300

    feat: docker-compose backend, frontend and database

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '12345678@',
-  database: 'cashforce-db',
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: Number(process.env.MYSQL_PORT) || 3306,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: 'cashforce',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
